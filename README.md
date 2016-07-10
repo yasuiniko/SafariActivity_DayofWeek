44487632005d49fbeee64837c3b739d2b312f7a9
Jason R. Wang <accounts@jrwang.ca>
2016-07-10 19:40:41 +0100

    v0.1 with basic functionality.

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # SafariActivity_DayofWeek
 
-Extract activity from `History.db` and plot how active I am on different days of the week.
+Extract activity from `History.db` for OS X/macOS's Safari and plots how active I am on different days of the week.
+
+### Changelog
+v0.1	Basic functionality added:
+* Finds `History.db` file from `~/Library/Safari/` automatically
+* Will catch if the database cannot be opened
+* Converts NSDate timestamps for history data into day of the week
+	* Note that time zone differences are probably not correct since Safari likely looks to the system to determine the time and travelling users (like me) don't always update this immediately
+* Matlibplot histogram works but is ugly
+
--- main.py
+++ main.py
@@ -2,27 +2,51 @@
 
 # To plot average Safari activity for each day of the week.
 
-# SQLite help from http://zetcode.com/db/sqlitepythontutorial/
-import sqlite3 as lite
-import sys
 
+import sqlite3 as lite
+import numpy as np
+import pylab as pl
+import sys
+import datetime
+
+# Find the Safari history database which is in ~/Library/Safari/ by default
 from os.path import expanduser
 home = expanduser("~")
 #database = home + "/Library/Safari/History.db"
 database = home + "/Desktop/history.db"
 
+# Try to open the database
+# SQLite help from http://zetcode.com/db/sqlitepythontutorial/
 con = None
 
 try:
+
+    # For Testing
+    #print "Opened database successfully";
+
     con = lite.connect(database)
+    cur =  con.cursor()
+    
+    cur.execute('SELECT visit_time FROM history_visits;')
 
-    #cur =  con.cursor()
-    #cur.execute('SELECT visit_time FROM history_visits;')
+    data = [int(data[0]) for data in cur.fetchall()]
+    # Convert visit_time from NSDate format to UNIX time
+    data = map(lambda x: x + 978307200, data)
+    data = np.asarray(data, 'datetime64[s]')
+    data = data.tolist()
 
-    #data = cur.fetchone()
+    # Find the day of week where 0 is Monday and 6 is Sunday
+    dow = map(lambda x: x.weekday(), data)
 
-    #print "%s" % data
-    print "Opened database successfully";
+    # Plot a histogram of the results
+    names = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    pl.hist(dow, bins = [0,1,2,3,4,5,6,7], align = "left")
+    pl.xlabel('Day of Week')
+    pl.xticks([0,1,2,3,4,5,6,7], names, size = "small")
+    pl.ylabel('Number of Websites Visited')
+    pl.show()
+
+    # To split analysis up into weeks, check for when a value is less than the previous.
 
 except lite.Error, e:
     
