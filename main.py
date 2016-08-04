5d94791d2b5a2205f58f6a46cb5eab61b5004674
Jason R. Wang <accounts@jrwang.ca>
2016-08-04 12:56:53 +0100

    Output in percent rather than raw values.

--- main.py
+++ main.py
@@ -2,12 +2,13 @@
 
 # To plot average Safari activity for each day of the week.
 
-
+import matplotlib
 import sqlite3 as lite
 import numpy as np
 import pylab as pl
 import sys
 import datetime
+from matplotlib.ticker import FuncFormatter
 
 # Find the Safari history database which is in ~/Library/Safari/ by default
 from os.path import expanduser
@@ -35,14 +36,33 @@ try:
     data = data.tolist()
 
     # Find the day of week where 0 is Monday and 6 is Sunday
-    dow = map(lambda x: x.weekday(), data)
+    day_of_week = map(lambda x: x.weekday(), data)
 
     # Plot a histogram of the results
     names = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    pl.hist(dow, bins = [0,1,2,3,4,5,6,7], align = "left")
+    pl.hist(day_of_week, bins = [0,1,2,3,4,5,6,7], align = "left", normed = 'true')
     pl.xlabel('Day of Week')
     pl.xticks([0,1,2,3,4,5,6,7], names, size = "small")
-    pl.ylabel('Number of Websites Visited')
+    pl.ylabel('Proportion of All Activity / %')
+
+    def to_percent(y, position):
+        # Ignore the passed in position. This has the effect of scaling the default
+        # tick locations.
+        s = str(100 * y)
+
+        # The percent symbol needs escaping in latex
+        if matplotlib.rcParams['text.usetex'] is True:
+            return s + r'$\%$'
+        else:
+            return s + '%'
+
+    # Create the formatter using the function to_percent. This multiplies all the
+    # default labels by 100, making them all percentages
+    formatter = FuncFormatter(to_percent)
+
+    # Set the formatter
+    pl.gca().yaxis.set_major_formatter(formatter)
+    
     pl.show()
 
     # To split analysis up into weeks, check for when a value is less than the previous.
